f86e1794ee3ecff6067c8e82dbc429522971cc3b
CodingCas <casweeno2000@gmail.com>
2022-08-26 16:19:37 +0100

    updated contract

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,90 +2,77 @@
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract Auction {
-    IERC721 NFTaddress;
-    bool started;
-    bool ended;
-    address seller;
-    string NFTname;
+contract AuctionCas {
+    IERC721 ItemAddress;
+    uint itemId;
+    string itemName;
+    bool AuctionStarted;
+    bool AuctionEnded;
+    uint highestBidAmount;
     address highestBidder;
-    uint256 NFTId;
-    uint256 TimeEnded;
-    uint256 highestBid;
-    uint256 lastcaller;
-    uint256 endAt;
 
-    struct Bid{
-        uint256 amount;
-        uint256 timeOfBid;
-    }
+    address itemSeller;
 
-    mapping(address=> Bid) bidder;
+    uint auctionPeriod = block.timestamp + 24 days;
 
-    constructor(address _NFTaddress, string memory _NFTname, uint256 _NFTId){
-        NFTaddress = IERC721(_NFTaddress);
-        require(msg.sender == NFTaddress.ownerOf(_NFTId), "You are not an owner of these NFT");
-        seller = msg.sender;
-        NFTname = _NFTname;
-        NFTId = _NFTId;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == seller, "You are not a seller");
+    modifier onlyOwner() {
+        require(msg.sender == itemSeller);
         _;
     }
 
-    function StartAuction(uint256 _minBid) public onlyOwner {
-        require(msg.sender == seller, "Not the right seller");
+    mapping(address => uint) bidders;
 
-        NFTaddress.transferFrom(msg.sender, address(this), NFTId);
-        require(!started, "The Auction already started");
-
-        highestBid = _minBid;
-        started = true;
-
-        lastcaller = block.timestamp + 20 minutes;
-        endAt = block.timestamp + 12 hours;
+    constructor(address _itemAddress, string memory _itemName, uint _itemId) {
+        ItemAddress = IERC721(_itemAddress);
+        require(msg.sender == ItemAddress.ownerOf(_itemId));
+        itemSeller = msg.sender;
+        itemName = _itemName;
+        itemId = _itemId;
     }
 
-    function PlaceBid() public payable{
-        if(block.timestamp >= lastcaller + 5 minutes | endAt){
+    function startAuction(uint _minBidAmount) external onlyOwner {
+       require(!AuctionStarted, "Auction already started");
+
+        ItemAddress.transferFrom(msg.sender, address(this), itemId);
+
+        highestBidAmount = _minBidAmount;
+        AuctionStarted = true;
+    }
+
+    function bid() external payable {
+        require(AuctionStarted == true, "Can't bid now, auction hasn't started yet");
+        require(msg.value > highestBidAmount, "can't bid less than highest bid");
+
+        if(block.timestamp > auctionPeriod) {
             endAuction();
-        }
-        else{
-            require(started, "Auction have not started or ended");
-            require(msg.value > highestBid, "There's an higher bid");
-
-            Bid storage bid = bidder[msg.sender];
-
-            bid.amount += msg.value;
-            bid.timeOfBid = block.timestamp;
-
-            highestBid = bid.amount;
+        } else {
+            bidders[msg.sender] += msg.value;
+            highestBidAmount = msg.value;
             highestBidder = msg.sender;
-            lastcaller = bid.timeOfBid;
         }
     }
 
-    function endAuction() internal {
-        require(!ended, "The Auction has ended");
-        require(NFTaddress.ownerOf(NFTId) == address(this), "The NFT has been trabsferred to the highest bidder");
-        
-        NFTaddress.safeTransferFrom(address(this), highestBidder, NFTId);
-        uint256 winnerValue = bidder[highestBidder].amount;
-        bidder[highestBidder].amount = 0;
-        payable(seller).transfer(winnerValue);
-        ended = true;
+    function endAuction() public onlyOwner {
+        require(AuctionEnded == false, "Auction already ended");
+        require(bidders[highestBidder] == highestBidAmount, "This doesn't correlate");
 
+        uint winningAmount = bidders[highestBidder];
+
+        bidders[highestBidder] = 0;
+
+        payable(itemSeller).transfer(winningAmount);
+        ItemAddress.safeTransferFrom(address(this), highestBidder, itemId);
+
+        AuctionEnded = true;
     }
 
-    function withdraw() public {
-        require(NFTaddress.ownerOf(NFTId) == address(this), "The NFT has been trabsferred to the highest bidder");
-        require(msg.sender != address(0), "can't send to zero address");
-        require(ended, "You can only withdraw after the auction ended");
-        uint256 userValue = bidder[msg.sender].amount;
-        bidder[msg.sender].amount = 0;
-        payable(msg.sender).transfer(userValue);
+    function withdraw() external {
+        require(AuctionEnded == true, "You can't withdraw until action ends");
+        require(bidders[msg.sender] > 0, "You didn't bid here");
 
+        uint biddedAmount = bidders[msg.sender];
+        bidders[msg.sender] = 0;
+
+        payable(msg.sender).transfer(biddedAmount);
     }
 }
